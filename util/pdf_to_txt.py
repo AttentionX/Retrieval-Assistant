f28da8520495e45b3e2ef102f284508b30332036
Adam <cmlee3211@gmail.com>
2023-03-16 00:24:00 +0900

    Add files and logic for getting texts for each page

---
+++ util/pdf_to_txt.py
@@ -0,0 +1,21 @@
+import PyPDF2
+ 
+file_path = '../samples/papers/transformer.pdf'
+ 
+# Create file object variable
+# Opening method will be rb
+pdf_file = open(file_path,'rb')
+ 
+# Create reader variable that will read the pdffileobj
+pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+ 
+# This will store the number of pages of this pdf file
+num_pages = pdf_reader.numPages
+
+# Print the text of each page in the pdf file
+for page in range(num_pages):
+    text = pdf_reader.getPage(page).extractText()
+    print(text)
+ 
+# Close the file object
+pdf_file.close()
