67930e05291af8ea96bd6f792f91c7024684c11d
Adam Lee <adamlee@Adams-MacBook-Pro.local>
2023-03-16 15:29:22 +0900

    Fix

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ chat_history = []
 
 fileContent = process.max_length(fileContent, 15000)
 
-chat_instruction = "You are a helpful assistant. You can answer questions soley based on the information given below. If the question can't be answered with the information given by the user, tell the user that you were unable to find the answer from the given information and ask the user if you should answer the question without referring to the given information."
 examples = '''
 For example, if the user asks a question that can't be answered with the given information, respond as follows:
 User: What is the GPT-10 architecture?
